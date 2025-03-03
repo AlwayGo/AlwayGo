550f2401b9805db8b79fa21ac9938fa4f8af00cf
vasifgumbatov <gumbatovvasif4@gmail.com>
2025-03-03 19:52:19 +0400

    Gradle files added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    id("androidx.navigation.safeargs.kotlin")
+    kotlin("kapt")
 }
 
 android {
@@ -10,6 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.alwaygo.alwaygo"
         minSdk = 23
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,4 +49,45 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Navigation
+    implementation(libs.bundles.navigationLibs)
+
+    implementation(libs.bundles.networking)
+
+    // Room
+    implementation(libs.room.runtime)
+    //noinspection KaptUsageInsteadOfKsp
+    kapt(libs.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // ViewModel && LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Glide
+    implementation(libs.coil)
+    implementation(libs.glide)
+    //noinspection KaptUsageInsteadOfKsp
+    kapt(libs.glide.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Hilt
+    implementation(libs.hilt)
+    kapt(libs.hiltCompiler)
+
+    // Paging
+    implementation(libs.paging3)
+    implementation(libs.androidx.paging.runtime.v311)
+    implementation(libs.volley)
+
+    // Splash screen
+    implementation(libs.androidx.core.splashscreen)
+
+    // SwipeRefreshLayout
+    implementation(libs.androidx.swiperefreshlayout)
+
 }
